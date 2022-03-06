4c22b001a0ac2a9cb98c25eb872cf9d2c640703e
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-03-06 19:48:34 -0300

    a few fixes + calculator almost done [no theme still]

--- index.js
+++ index.js
@@ -13,11 +13,47 @@ function spClass(key, item) {
             item.val("");
             break;
         case "DEL":
-            item.val(item.val().slice(0, item.val().length - 1));
+            if (
+                item.val().slice(item.val().length - 1, item.val().length) ===
+                " "
+            ) {
+                item.val(item.val().slice(0, item.val().length - 3));
+            } else {
+                item.val(item.val().slice(0, item.val().length - 1));
+            }
+            break;
+        case "=":
+            result(item.val());
             break;
 
         default:
-            console.error(ErrorEvent + " sth wrong bro");
+            let sTemp, temp;
+
+            temp = item.val().slice(item.val().length - 1, item.val().length);
+            /* cuz it returns string */
+            if (
+                temp == "+" ||
+                temp == "-" ||
+                temp == "/" ||
+                temp == "x" ||
+                temp == "." ||
+                temp == " "
+            ) {
+                sTemp = true;
+            } else {
+                sTemp = false;
+            }
+
+            // Every time I use a operator it adds an space
+            if (sTemp) return;
+
+            if (key == ".") {
+                item.val(item.val() + key);
+            } else {
+                item.val(item.val() + " " + key + " ");
+            }
             break;
     }
 }
+
+function result(item) {}
